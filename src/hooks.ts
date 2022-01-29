6b09d19c683c41bced8759e81eed220a055dd49e
Anwesh43 <anweshthecool0@gmail.com>
2022-01-29 20:53:14 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -45,3 +45,30 @@ export const useDimension = () => {
         h
     }
 }
+
+const sinify = (scale : number) => Math.sin(scale * Math.PI)
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const background = 'indigo'
+    const size = Math.min(w, h) / 10 
+    const hSize = size / 3 
+    const sf : number = sinify(scale)
+    const position = 'absolute'
+    return {
+        barStyle(i : number) {
+            const sk : number = 1 - 2 * i
+            const left = `${(w / 2 - size / 2) * (1 + sk * sf)}px`
+            const top = `${(h / 2 - hSize / 2) * (1 + sf)}px`
+            const width = `${size}px`
+            const height = `${hSize}px`
+            return {
+                position,
+                width, 
+                height, 
+                left, 
+                top,
+                background 
+            }
+        }
+    }
+}
